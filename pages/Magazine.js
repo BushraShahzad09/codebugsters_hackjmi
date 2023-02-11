3404fa650d549701bb9032a5376a136855a2636b
Bushra Shahzad <bushra05shahzad09>
2023-02-12 00:59:07 +0530

    magazine changed

--- pages/Magazine.js
+++ pages/Magazine.js
@@ -12,7 +12,27 @@ const Magazine = () => {
     }}>
       <div className="flex place-content-center items-center align-middle h-full">
         <HTMLFlipBook width={500} height={800} ref={pagee}>
-          <div className='bg-white border-black px-2 py-2 border-solid border-4 text-center items-center'><h1 className='font-mono text-xl font-normal'>Read Interesting <br /> Articles</h1><img src='/tree.gif' alt='cover' className='mx-auto mt-40' /><h1 className='font-mono text-xl font-normal pt-24'>The Speaking Tree</h1></div>
+          <div className='bg-white border-black px-2 py-2 border-solid border-4 text-center items-center rounded-lg'><h1 className='font-mono text-xl font-bold py-8'>Read Interesting <br /> Articles</h1><img src='/tree.gif' alt='cover' className='mx-auto mt-40' /><h1 className='font-mono text-xl font-bold pt-24'>The Speaking Tree</h1></div>
+          <div className='bg-teal-200 text-black border-black px-2 py-2 border-solid border-4 rounded-lg '><div className='text-center font-bold text-2xl py-1'>Brush Up Fin!</div><p className='py-11'>
+            <div className="py-2  font-bold">&#8377; Income </div>
+            &#9734; List all sources of money that you receive in a month. <br />
+            &#9734; It includes paychecks, investment income and money that you make from side jobs.<br />
+            &#9734; You should find ways to increase your income morally.<br />
+            <div className="py-2  font-bold">&#8377; Expenses </div>
+            &#9734; List all purachases that you receive in a month. <br />
+            &#9734; It is advisable to cut down on unnecessary spendings.<br />
+            <div className="py-2  font-bold">&#8377; Budgeting  </div>
+            &#9734; Keeping track of one’s spending patterns.
+            <div className="py-2 font-bold">&#8377; Debt </div>
+            &#9734; A sum of money that is owed or due.<br />
+            &#9734; Borrowing money for goods that are required to make a livelihood is considered good debt.<br />
+            &#9734; Borrowing money for unneeded expenses is considered bad debt.
+            <div className="py-2  font-bold">&#8377; Savings  </div>
+            &#9734; Savings ensure financial security and a bright future.
+            <div className="py-2  font-bold">&#8377; Investment  </div>
+            &#9734; Investments assist in the generation of additional monthly income. <br />
+            &#9734; Mutual funds, real estate, and gold are some of the most popular investment possibilities.
+          </p></div>
           <div className='bg-orange-200 text-black border-black px-2 py-2 border-solid border-4 rounded-lg '><div className='text-center font-bold text-2xl py-1'>50/20/30 Budget Rule</div><p className='py-11'>
             <div className="text-center py-3 text-xl font-bold">&#8377; 50% Needs</div>
             &#9734; Needs are those bills that you absolutely necessary for survival.<br />
@@ -45,36 +65,28 @@ const Magazine = () => {
             <div className="py-2  font-bold">&#8377; Cash Flow:  </div>
             It is the net balance of cash moving in and out of a business at a specific point in time.
           </p></div>
-          <div className='bg-teal-200 text-black border-black px-2 py-2 border-solid border-4 rounded-lg '><div className='text-center font-bold text-2xl py-1'>Brush Up Fin!</div><p className='py-11'>
-          <div className="py-2  font-bold">&#8377; Income </div>
-            &#9734; List all sources of money that you receive in a month. <br/>
-            &#9734; It includes paychecks, investment income and money that you make from side jobs.<br/>
-            &#9734; You should find ways to increase your income morally.<br/>
-            <div className="py-2  font-bold">&#8377; Expenses </div>
-            &#9734; List all purachases that you receive in a month. <br/>
-            &#9734; It is advisable to cut down on unnecessary spendings.<br/>
+          
+          <div className='bg-slate-300 text-black border-black px-2 py-2 border-solid border-4 rounded-lg '><div className='text-center font-bold text-2xl py-1'>Stocketing!</div><p className='py-11'>
+            <div className="py-2 font-bold">&#8377; Stocks </div>
+            &#9734; When you invest in stocks, you get a slice of ownership in a company that can yield returns if it's successful. <br />
+            <div className="py-2 font-bold">&#8377; Goals & Budget </div>
+            &#9734; Review your financial situation( After-tax income, expenses, debt, net worth, risk tolerance ).<br />
+            &#9734; Set clear financial goals.<br />
+            <div className="py-2 font-bold">&#8377; Get acquainted with stocks </div>
+            &#9734; Different types of stocks to choose from such as blue-chip stocks, growth stocks, and penny stocks.<br />
+            &#9734; Different-sized companies with varying market capitalizations and degrees of risk ( Small-cap, mid-cap, or large-cap stocks ) <br />
+            &#9734; Explore exchange-traded funds (ETFs) ,mutual funds, index funds<br />
+            <div className="py-2 font-bold">&#8377; Investing Strategy </div>
+            &#9734; Passive investing —  buy-and-hold approach.<br />
+            &#9734; Active investing —  buying and selling based on market conditions.<br />
+            <div className="py-2 font-bold">&#8377; Investing Account & Portfolio </div>
+            &#9734; Decide if you want to do it yourself or get a professional to help out.<br />
+            &#9734; Diversify your portfolio by investing in different types of investment vehicles and industries.<br />
 
 
-            <div className="py-2  font-bold">&#8377; Budgeting  </div>
-            &#9734; Keeping track of one’s spending patterns.
-                 <div className="py-2 font-bold">&#8377; Debt </div>
-                 &#9734; A sum of money that is owed or due.<br/>
-                 &#9734; Borrowing money for goods that are required to make a livelihood is considered good debt.<br/>
-                 &#9734; Borrowing money for unneeded expenses is considered bad debt.
-            <div className="py-2  font-bold">&#8377; Savings  </div>
-            &#9734; Savings ensure financial security and a bright future.
-            <div className="py-2  font-bold">&#8377; Investment  </div>
-            &#9734; Investments assist in the generation of additional monthly income. <br/>
-            &#9734; Mutual funds, real estate, and gold are some of the most popular investment possibilities.
-            
-            
-            
+
           </p></div>
 
-          <div className=' text-green-700 bg-white border-black border-4 px-2 py-2 border-solid  text-center'><span className='text-xl font-bold '>Page 2 content</span><p className='py-11'>This is where the content goes. We can use some api to request articles and display them here </p></div>
-          <div className='bg-green-700 text-white border-black px-2 py-2 border-solid border-4 text-center'><span className='text-xl font-bold '>Page 3 content</span><p className='py-11'>This is where the content goes. We can use some api to request articles and display them here </p></div>
-          <div className='bg-green-700 text-white border-black px-2 py-2 border-solid border-4 text-center'><span className='text-xl font-bold '>Page 4 content</span><p className='py-11'>This is where the content goes. We can use some api to request articles and display them here </p></div>
-          <div className='bg-green-700 text-white border-black px-2 py-2 border-solid border-4 text-center'><span className='text-xl font-bold '>Page 5 content</span><p className='py-11'>This is where the content goes. We can use some api to request articles and display them here </p></div>
         </HTMLFlipBook>
 
         <div className="flex justify-between p-4 w-full bottom-0">
