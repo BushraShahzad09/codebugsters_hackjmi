5dedbbc96d41204cdf111ec4df92327470c4f7b0
jasp69 <h.munib23@gmail.com>
2023-02-12 11:09:20 +0530

    UI changes

---
+++ pages/darkmode.js
@@ -0,0 +1,6 @@
+// const toggleDarkModeBtn = document.getElementById("toggle-dark-mode-btn");
+// const body = document.querySelector("body");
+
+// toggleDarkModeBtn.addEventListener("click", function () {
+//   body.classList.toggle("dark-mode");
+// });
